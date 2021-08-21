dc22341b1eb113a7f0dc962b1efc6820d9af02d5
CamilaArroyo <camiarroyo98.1@gmail.com>
2021-08-21 17:31:56 -0300

    Nueva nav y sección index.html

---
+++ js/index.js
@@ -0,0 +1,46 @@
+
+$("#btnServicio1").click(function (e) { 
+    $("#imgServicio1").slideUp(600);
+    $("#infoServicio1").slideDown();
+});
+
+$("#btnOffServicio1").click(function (e) { 
+    $("#infoServicio1").slideUp(600);
+    $("#imgServicio1").slideDown(600);
+});
+
+
+
+$("#btnServicio2").click(function (e) { 
+    $("#imgServicio2").slideUp(600);
+    $("#infoServicio2").slideDown();
+});
+
+$("#btnOffServicio2").click(function (e) { 
+    $("#infoServicio2").slideUp(600);
+    $("#imgServicio2").slideDown(600);
+});
+
+
+
+$("#btnServicio3").click(function (e) { 
+    $("#imgServicio3").slideUp(600);
+    $("#infoServicio3").slideDown();
+});
+
+$("#btnOffServicio3").click(function (e) { 
+    $("#infoServicio3").slideUp(600);
+    $("#imgServicio3").slideDown(600);
+});
+
+
+
+$("#btnServicio4").click(function (e) { 
+    $("#imgServicio4").slideUp(600);
+    $("#infoServicio4").slideDown();
+});
+
+$("#btnOffServicio4").click(function (e) { 
+    $("#infoServicio4").slideUp(600);
+    $("#imgServicio4").slideDown(600);
+});
